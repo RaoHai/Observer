953a8b113510b28f36815a2313388ec6ad17010a
surgesoft <surgesoft@gmail.com>
2014-04-10 19:19:02 +0800

    - -

--- content/assets/js/models.js
+++ content/assets/js/models.js
@@ -27,36 +27,3 @@
         sync: wrapSync
     });
 }());
-/*global window, document, Observer, $, _, Backbone */
-(function () {
-    'use strict';
-
-    Observer.Models.Brand = Observer.ProgressModel.extend({
-
-    });
-
-    Observer.Collections.Brands = Backbone.Collection.extend({
-
-      url: '/brands/',
-      model: Observer.Models.Brand,
-
-    });
-
-}());
-
-/*global window, document, Observer, $, _, Backbone */
-(function () {
-    'use strict';
-
-    Observer.Models.Training = Observer.ProgressModel.extend({
-
-    });
-
-    Observer.Collections.Trainings = Backbone.Collection.extend({
-
-      url: '/trainings/',
-      model: Observer.Models.Training,
-
-    });
-
-}());
